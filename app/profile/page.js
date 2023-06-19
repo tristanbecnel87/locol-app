3cd2469bec402a395ad791d4230b083092c91718
Shaz Momin <shaz.momin@utexas.edu>
2023-06-19 15:59:00 -0500

    fixed aws auth config error

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,6 +1,9 @@
 'use client';
 import React, { useState, useEffect } from 'react'
 import { Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
+import config from 'app/aws-exports';
+Amplify.configure(config);
 
 const Page = () => {
     const [user, setUser] = useState(null)
@@ -10,10 +13,9 @@ const Page = () => {
             try {
                 const currentUser = await Auth.currentAuthenticatedUser();
                 const attributes = currentUser.attributes;
-                console.log("User attributes:", attributes);
+                console.log(currentUser);
                 setUser(currentUser);
         
-                router.push('/dashboard')
             } catch (error) {
                 console.error("Failed to fetch user attributes.", error);
             }
@@ -23,7 +25,7 @@ const Page = () => {
     }, [])
 
     return (
-        <div>Profile Page for {user}</div>
+        <div>Profile Page for {user?.attributes.email}</div>
     )
 }
 
