d39b897a0148ede87962027ba9645e81fad6999f
--replace-all <tristanbecnel87@gmail.com>
2023-07-02 14:32:40 -0500

    users can upload a portfolio, kinda janky

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect} from 'react'
 import { Auth } from 'aws-amplify';
 import { Amplify } from 'aws-amplify';
 import config from 'app/aws-exports';
@@ -7,6 +7,7 @@ import { Storage } from "aws-amplify";
 Amplify.configure(config);
 import "@aws-amplify/ui-react/styles.css";
 import {useRouter} from "next/navigation";
+import {useRef} from "react";
 
 const Page = () => {
     const router = useRouter()
@@ -14,6 +15,9 @@ const Page = () => {
     const [fileData, setFileData] = useState();
     const [fileStatus, setFileStatus] = useState(false);
     const [imageUrl, setImageUrl] = useState(null);
+    const [documentUrl, setDocumentUrl] = useState(null);
+    const [isDocumentLoaded, setIsDocumentLoaded] = useState(false);
+    const pdfViewerRef = useRef();
 
 
 
@@ -27,7 +31,9 @@ const Page = () => {
 
                  // Retrieve the image URL
                  const imageUrl = await getImageUrl(currentUser.attributes.sub, 'pfp');
-                setImageUrl(imageUrl);
+                 const document = await getDocumentUrl(currentUser.attributes.sub, 'portfolio');
+                 setImageUrl(imageUrl);
+                 setDocumentUrl(document);
             } catch (error) {
                 console.error("Failed to fetch user attributes.", error);
                 router.push('/auth/signIn')
@@ -46,8 +52,17 @@ const Page = () => {
       throw error;
     }
   };
+       const getDocumentUrl = async (userId, fileName) => {
+    try {
+      const documentUrl = await Storage.get(`${userId}/${fileName}`);
+      return documentUrl;
+    } catch (error) {
+      console.error('Error retrieving portfolio:', error);
+      throw error;
+    }
+  };
 
-     const uploadFile = async () => { //profile picture
+     const uploadImage = async () => { //profile picture
         try {
             const currentUser = await Auth.currentAuthenticatedUser();
             const userId = currentUser.attributes.sub;
@@ -65,6 +80,34 @@ const Page = () => {
             console.error("Error uploading file:", error);
         }
   };
+      const uploadDocument = async () => {
+    try {
+      const currentUser = await Auth.currentAuthenticatedUser();
+      const userId = currentUser.attributes.sub;
+
+      const result = await Storage.put(`${userId}/portfolio`, fileData, {
+        contentType: fileData.type,
+      });
+      setFileStatus(true);
+      console.log('Portfolio uploaded successfully:', result);
+
+      // Retrieve the updated document URL
+      const updatedDocumentUrl = await getDocumentUrl(userId, 'portfolio');
+      setDocumentUrl(updatedDocumentUrl);
+    } catch (error) {
+      console.error('Portfolio uploading document:', error);
+    }
+  };
+      const previewDocument = () => {
+    if (isDocumentLoaded) {
+      // Document is already loaded, just focus on the viewer
+      pdfViewerRef.current?.focus();
+    } else {
+      // Document is not loaded, open in a new tab and set the loaded flag
+      window.open(documentUrl, '_blank');
+      setIsDocumentLoaded(true);
+    }
+  };
 
 return (
   <div>
@@ -73,11 +116,27 @@ return (
       <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
     </div>
     <div>
-      <button onClick={uploadFile}>Upload file</button>
+      <button onClick={uploadImage}>Upload Profile Picture</button>
     </div>
+      <div>
+          <button onClick={uploadDocument}>Upload Portfolio</button>
+      </div>
+
     {fileStatus ? "File uploaded successfully" : ""}
     {imageUrl && <img src={imageUrl} alt="Profile Picture" />}
-  </div>
+      {documentUrl && (
+        <div>
+          <h3>Portfolio Preview</h3>
+          <button onClick={previewDocument}>Download Portfolio</button>
+          <div
+            className="pdf-viewer"
+            style={{ width: '100%', height: '500px', border: '1px solid black' }}
+            ref={pdfViewerRef}
+            tabIndex="0"
+          ></div>
+        </div>
+      )}
+    </div>
 );
 }
 
